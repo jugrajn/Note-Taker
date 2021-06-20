a0e3dc9ae332d19d30ac04e3500711c8e53aac64
jugrajn <jugraj.nijjar@gmail.com>
2021-06-20 01:55:10 -0400

    created routes for root, notes.html, and api for notes json data

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 //Dependencies
 const express = require('express');
 const fs =require('fs');
+const path = require('path');
 
 //App to handle reqests
 const app = express();
+
+//Create Port
+const PORT = 3000;
+
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//Create Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.JSON(notes));
