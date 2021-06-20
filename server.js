fd2d2bb313f42ca40237c7479773a554ff25ff81
jugrajn <jugraj.nijjar@gmail.com>
2021-06-20 15:02:33 -0400

    fixex port issue

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 //Create Port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true}));
@@ -19,3 +19,8 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.JSON(notes));
+
+
+
+// Get the server to start listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
